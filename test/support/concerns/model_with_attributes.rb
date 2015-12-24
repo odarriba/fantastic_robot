3ab6b43d8445c8cc9105761bce2e22254981aee4
Óscar de Arriba <oscar.dearriba@the-cocktail.com>
2015-12-24 12:29:41 +0100

    Add extra info for model_with_attributes concern tests.

--- test/support/concerns/model_with_attributes.rb
+++ test/support/concerns/model_with_attributes.rb
@@ -11,10 +11,10 @@ module ModelWithAttributes
 
       attribute_list.each do |attrib, value|
         # Test that the attribute is readable
-        assert_respond_to(@subject, attrib)
+        assert_respond_to(@subject, attrib, "Error while testing attribute :#{attrib}")
 
         # Test that the attribute returns it's value
-        assert_equal(@subject.send(attrib), value)
+        assert_equal(@subject.send(attrib), value, "Error while testing attribute :#{attrib}")
       end
     end
   end
