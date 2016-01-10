87d28441d6b1d954449924ae9299dc19324ecec3
Óscar de Arriba <oscar.dearriba@the-cocktail.com>
2016-01-10 17:19:32 +0100

    Add generators for configuration files.

---
+++ lib/generators/telegram_bot/files/telegram_bot.rb
@@ -0,0 +1,10 @@
+TelegramBot.configure do |config|
+  # Secret token of the API
+  config.api_key = 'your_bot_api_token'
+
+  # Delivery method (can be :polling or :webhook)
+  config.delivery_method = :webhook
+
+  # Callback URL of the webhook
+  config.callback_url = 'http://your_app.dev/receive'
+end
