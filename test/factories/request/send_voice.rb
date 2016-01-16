425c50be7b1f273632a64615a54da28bed68dbce
Óscar de Arriba <oscar.dearriba@the-cocktail.com>
2016-01-16 13:48:59 +0100

    Add tests for sendVoice request model.

--- test/factories/request/send_voice.rb
+++ test/factories/request/send_voice.rb
@@ -1,17 +1,15 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
-  factory :send_video, class: TelegramBot::Request::SendVideo do
+  factory :send_voice, class: TelegramBot::Request::SendVoice do
     chat_id { Faker::Number.number(10) }
-    video { Faker::Lorem.word }
+    voice { Faker::Lorem.word }
     duration { Faker::Number.number(3) }
-    caption { Faker::Lorem.sentence }
     reply_to_message_id { Faker::Number.number(10) }
     reply_markup { nil }
 
-    factory :send_video_without_optional_fields do
+    factory :send_voice_without_optional_fields do
       duration { nil }
-      caption { nil }
       reply_to_message_id { nil }
       reply_markup { nil }
     end
