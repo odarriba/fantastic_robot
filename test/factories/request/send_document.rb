65f3a1f405af1a34d95b3d0149b7fa1e6c02f15a
Óscar de Arriba <oscar.dearriba@the-cocktail.com>
2016-01-16 13:24:55 +0100

    Add tests for sendDocument request model.

---
+++ test/factories/request/send_document.rb
@@ -0,0 +1,15 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :send_document, class: TelegramBot::Request::SendDocument do
+    chat_id { Faker::Number.number(10) }
+    document { Faker::Lorem.word }
+    reply_to_message_id { Faker::Number.number(10) }
+    reply_markup { nil }
+
+    factory :send_document_without_optional_fields do
+      reply_to_message_id { nil }
+      reply_markup { nil }
+    end
+  end
+end
